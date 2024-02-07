ab1a020f3cc6e2835c51763118e840fa94190ac0
h0wter <91832203+h0wter@users.noreply.github.com>
2024-02-07 17:27:48 +0200

    add show/hide button to the date-picker component and update the component to save data to the search parameters

--- src/components/calendar/calendar.tsx
+++ src/components/calendar/calendar.tsx
@@ -1,19 +1,13 @@
-import { useState } from "react";
 import { Slider } from "./components/slider/slider.tsx";
 import { DatePicker } from "./components/date-picker/date-picker.tsx";
-import { type DateValue } from "./types/date-value.type.ts";
+import styles from "./styles.module.scss";
 
 const Calendar = () => {
-  const [selectedDate, setSelectedDate] = useState<DateValue>(new Date());
-
   return (
-    <>
+    <div className={styles.calendarContainer}>
       <Slider />
-      <DatePicker
-        selectedDate={selectedDate}
-        setSelectedDate={setSelectedDate}
-      />
-    </>
+      <DatePicker />
+    </div>
   );
 };
 
--- src/components/calendar/components/date-picker/date-picker.tsx
+++ src/components/calendar/components/date-picker/date-picker.tsx
@@ -1,28 +1,74 @@
+import { useCallback, useState } from "react";
 import Calendar from "react-calendar";
+import dayjs from "dayjs";
+import { useSearchParams } from "react-router-dom";
+import CaretRightIcon from "../../../../assets/images/icons/caret-right.svg?react";
+import CalendarIcon from "../../../../assets/images/icons/calendar.svg?react";
 import { getFormatterMonthYear } from "./helpers/get-formatted-month-year.helper.ts";
-import { DateValue } from "../../types/date-value.type.ts";
+import { getValidClassNames } from "../../../../helpers/get-valid-class-names.helper.ts";
+import { type DateValue } from "../../types/date-value.type.ts";
 import styles from "./styles.module.scss";
+import { compareDates } from "../slider/helpers/compare-dates.helper.ts";
 
-type Props = {
-  selectedDate: DateValue;
-  setSelectedDate: React.Dispatch<React.SetStateAction<DateValue>>;
-};
+const DatePicker = () => {
+  const [isDatePickedOpened, setIsDatePickedOpened] = useState(true);
+  const [searchParams, setSearchParams] = useSearchParams();
 
-const DatePicker = ({ selectedDate, setSelectedDate }: Props) => {
+  const dateFromUrl = searchParams.get("date") || new Date();
+  const decodedDate = dayjs(dateFromUrl).toDate();
+
+  const handleDatePickerToggle = useCallback(
+    () => setIsDatePickedOpened((prevState) => !prevState),
+    []
+  );
+
+  const handleDataChange = useCallback(
+    (dateValue: DateValue) => {
+      const date = Array.isArray(dateValue) ? dateValue[0] : dateValue;
+
+      const formattedDate = dayjs(date).format("MM-DD-YY");
+
+      if (date && !compareDates(date, new Date())) {
+        setSearchParams({ date: formattedDate });
+      } else {
+        setSearchParams();
+      }
+    },
+    [setSearchParams]
+  );
+
   return (
-    <div className={styles.calendarContainer}>
-      <Calendar
-        value={selectedDate}
-        locale="en-US"
-        calendarType="iso8601"
-        minDetail="decade"
-        prev2Label={null}
-        next2Label={null}
-        showNeighboringMonth={false}
-        formatMonthYear={getFormatterMonthYear}
-        onChange={setSelectedDate}
-      />
-    </div>
+    <>
+      <button
+        className={styles.datePickerButton}
+        onClick={handleDatePickerToggle}
+      >
+        <div className={styles.buttonLabelWrapper}>
+          <CalendarIcon />
+          <span>View Calendar</span>
+        </div>
+        <CaretRightIcon
+          className={getValidClassNames(
+            isDatePickedOpened && styles.buttonActive
+          )}
+        />
+      </button>
+      {isDatePickedOpened && (
+        <div className={styles.calendarContainer}>
+          <Calendar
+            value={decodedDate}
+            locale="en-US"
+            calendarType="iso8601"
+            minDetail="decade"
+            prev2Label={null}
+            next2Label={null}
+            showNeighboringMonth={false}
+            formatMonthYear={getFormatterMonthYear}
+            onChange={handleDataChange}
+          />
+        </div>
+      )}
+    </>
   );
 };
 
